397e843503857acf771646a7fd5fc6c98bda7d89
djordjeilic17 <malterego@gmail.com>
2015-04-02 10:09:14 +0200

    Napravljena klasa Menjacnica sa potrebnim atributima

---
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -0,0 +1,32 @@
+package menjacnica;
+
+public class Valuta {
+
+	
+	public String naziv;
+	private String oznaka;
+	
+	
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public String getOznaka() {
+		return oznaka;
+	}
+	public void setOznaka(String oznaka) {
+		this.oznaka = oznaka;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
