ff5b46559deba7aa4936670a10e2e66b28485a2d
djordjeilic17 <malterego@gmail.com>
2015-04-02 10:19:00 +0200

    Uradjena je logicka kontrola

--- Menjacnica/src/menjacnica/Valuta.java
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -21,6 +21,42 @@ public class Valuta {
 	}
 	
 	
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((oznaka == null) ? 0 : oznaka.hashCode());
+		return result;
+	}
+	
+	
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (oznaka == null) {
+			if (other.oznaka != null)
+				return false;
+		} else if (!oznaka.equals(other.oznaka))
+			return false;
+		return true;
+	}
+	
+	
+	public String toString() {
+		return "Valuta [naziv=" + naziv + ", oznaka=" + oznaka + "]";
+	}
+	
+	
 	
 	
 	
